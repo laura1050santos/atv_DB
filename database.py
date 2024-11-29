c5c75c7bf5f4957b435c859899b1140de85f53f7
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2024-11-29 11:47:05 -0300

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-def init_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///exemplo.db"
-    with app.app_context(): #criar o banco de acordo com o app
-        db.init_app(app)
-        db.create_all()
